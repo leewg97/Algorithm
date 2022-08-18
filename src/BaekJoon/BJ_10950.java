87f18997dd70dcc9c190bea6f91e3cf0879dfcc3
leewg97 <leewg97@naver.com>
2022-08-19 00:07:00 +0900

    BOJ A + B

---
+++ src/BaekJoon/BJ_10950.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+// A + B
+public class BJ_10950 {
+
+    public static void main(String[] args) throws IOException {
+        // Scanner 사용
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        int arr[] = new int[T];
+
+        for(int i = 0; i < T; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[i] = a + b;
+        }
+        sc.close();
+
+        for(int j : arr){
+            System.out.println(j);
+        }
+    }
+}
