08a3b45683df1d973034deb74c52e991c654314b
leewg97 <leewg97@naver.com>
2022-08-19 00:10:03 +0900

    BOJ 최소, 최대 2

---
+++ src/BaekJoon/BJ_20053.java
@@ -0,0 +1,26 @@
+package BaekJoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 최소, 최대 2
+public class BJ_20053 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            Arrays.sort(arr);
+            System.out.println(arr[0] + " " + arr[N - 1]);
+        }
+    }
+}
