c1dd8eb45f4adc38f8ae1d8a6a4bfafb43d71f70
leewg97 <leewg97@naver.com>
2022-08-19 00:06:31 +0900

    BOJ ??!

---
+++ src/BaekJoon/BJ_10926.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+// ??!
+public class BJ_10926 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.nextLine();
+
+        if(id != null){
+            System.out.println(id + "??!");
+        } else {
+            System.out.println(id);
+        }
+
+    }
+}
