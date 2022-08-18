9962a79e866c4b9ee383ab280203153a5ee20f27
leewg97 <leewg97@naver.com>
2022-08-19 00:02:57 +0900

    BOJ N 찍기

---
+++ src/BaekJoon/BJ_2741.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+import java.io.*;
+
+// N 찍기
+public class BJ_2741 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        int i =  1;
+
+        while(i <= N){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
