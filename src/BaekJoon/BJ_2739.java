268c32c5db9e5e1517642541253033021883149d
leewg97 <leewg97@naver.com>
2022-08-19 00:02:28 +0900

    BOJ 구구단

---
+++ src/BaekJoon/BJ_2739.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+// 구구단
+public class BJ_2739 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        for(int i = 1; i < 10; i++){
+            System.out.println(x + " * " + i + " = " + (x*i));
+        }
+
+
+    }
+
+}
