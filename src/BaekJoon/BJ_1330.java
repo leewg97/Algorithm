a3a1852283e75a87be62e994eeb6488620b34de4
leewg97 <leewg97@naver.com>
2022-08-18 23:54:18 +0900

    BOJ 두 수 비교하기

---
+++ src/BaekJoon/BJ_1330.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+// 두 수 비교하기
+public class BJ_1330 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        scanner.close();
+
+        if(A > B){
+            System.out.println(">");
+        } else if (A < B){
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+
+    }
+
+}
