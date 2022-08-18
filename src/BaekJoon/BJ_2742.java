030e0f90852104ada07280c60bec96ac43a70123
leewg97 <leewg97@naver.com>
2022-08-19 00:03:19 +0900

    BOJ 기찍 N

---
+++ src/BaekJoon/BJ_2742.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 기찍 N
+public class BJ_2742 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        while(N > 0){
+            System.out.println(N);
+            N--;
+        }
+
+    }
+
+}
