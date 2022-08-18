54411a72c6a6267fcd4a3dbb3f0e47fc640d60f8
leewg97 <leewg97@naver.com>
2022-08-19 00:07:17 +0900

    BOJ A * B

---
+++ src/BaekJoon/BJ_10998.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+// A * B
+public class BJ_10998 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * B);
+
+    }
+
+}
