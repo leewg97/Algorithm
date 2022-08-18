7014430a5e8ad363b8a674af586fa9bf667c11fa
leewg97 <leewg97@naver.com>
2022-08-19 00:01:06 +0900

    BOJ 분해합

---
+++ src/BaekJoon/BJ_2231.java
@@ -0,0 +1,34 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 분해합
+public class BJ_2231 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for (int i=0; i <= n; i++){
+
+            int number = i;
+            int sum = 0;
+
+            while (number != 0){
+                sum += number % 10; // number 를 10으로 나눈 나머지를 sum 에 더함
+                number /= 10;       // number 를 10으로 나눈 몫을 다시 num 에 저장. 즉, 1의 자리부터 한칸씩 위로
+            }
+            if (sum + i == n){
+                result = i;
+                break;
+            }
+        }
+        System.out.println(result);
+
+    }
+}
