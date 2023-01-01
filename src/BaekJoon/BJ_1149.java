2516e34e7dfe34ee497501a3ba0cb47a58af703a
leewg97 <leewg97@naver.com>
2023-01-02 04:26:17 +0900

    BOJ RGB 거리

---
+++ src/BaekJoon/BJ_1149.java
@@ -0,0 +1,45 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// RGB 거리
+public class BJ_1149 {
+
+    public static void main(String[] args) throws IOException {
+
+        String inputData = """
+        8
+        71 39 44
+        32 83 55
+        51 37 63
+        89 29 100
+        83 58 11
+        65 13 15
+        47 25 29
+        60 66 19
+        """;
+        System.setIn(new ByteArrayInputStream(inputData.getBytes()));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp =  new int[n + 1][3];
+
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int g = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + r;
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + g;
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + b;
+        }
+        System.out.println(Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2])));
+    }
+
+}
