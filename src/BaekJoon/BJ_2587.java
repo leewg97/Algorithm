027609477101ef268bf7769034d1002aa2acd7be
leewg97 <leewg97@naver.com>
2023-01-03 04:17:32 +0900

    BOJ 대표값2

---
+++ src/BaekJoon/BJ_2587.java
@@ -0,0 +1,41 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// 대표값2
+public class BJ_2587 {
+
+    public static void main(String[] args) throws IOException {
+
+        String inputData = """
+        10
+        40
+        30
+        60
+        30
+        """;
+        System.setIn(new ByteArrayInputStream(inputData.getBytes()));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[5];
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[i] += num;
+            sum += num;
+        }
+
+        System.out.println(sum / 5);
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[2]);
+
+    }
+}
