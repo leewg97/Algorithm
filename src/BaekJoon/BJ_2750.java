4249e3c069576997cc0ab248f30e95a23c3036c2
leewg97 <leewg97@naver.com>
2023-01-03 04:17:38 +0900

    BOJ 수 정렬하기

---
+++ src/BaekJoon/BJ_2750.java
@@ -0,0 +1,49 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 수 정렬하기
+public class BJ_2750 {
+
+    public static void main(String[] args) throws IOException {
+
+        String inputData = """
+        5
+        5
+        2
+        3
+        4
+        1
+        """;
+        System.setIn(new ByteArrayInputStream(inputData.getBytes()));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+
+        // Arrays.sort(arr);
+
+        for (int result : arr) {
+            System.out.println(result);
+        }
+
+    }
+}
