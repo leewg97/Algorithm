03a500f0d806811c890e5760c47a2074b727cc38
leewg97 <leewg97@naver.com>
2023-01-03 04:18:13 +0900

    BOJ 커트라인

---
+++ src/BaekJoon/BJ_25305.java
@@ -0,0 +1,40 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 커트라인
+public class BJ_25305 {
+
+    public static void main(String[] args) throws IOException {
+
+        String inputData = """
+        5 2
+        100 76 85 93 98
+        """;
+
+        System.setIn(new ByteArrayInputStream(inputData.getBytes()));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[N - K]);
+
+    }
+
+}
