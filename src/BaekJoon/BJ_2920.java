522fbf0797ead84828cd9cee315f87758961bbc6
leewg97 <leewg97@naver.com>
2023-01-03 07:30:35 +0900

    BOJ 음계

--- src/BaekJoon/BJ_2920.java
+++ src/BaekJoon/BJ_2920.java
@@ -1,31 +1,32 @@
 package BaekJoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 // 음계
 public class BJ_2920 {
 
-    public static void main(String[] args) {
+    public static int[] ASC = {1, 2, 3, 4, 5, 6, 7, 8};
+    public static int[] DESC = {8, 7, 6, 5, 4, 3, 2, 1};
 
-        Scanner sc = new Scanner(System.in);
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[8];
-        String result = "";
 
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < arr.length - 1; i++){
-            if(arr[i+1] == arr[i] + 1){
-                result = "ascending";
-            } else if (arr[i+1] ==  arr[i] - 1) {
-                result ="descending";
-            } else {
-                result = "mixed";
-                break;
-            }
+        if (Arrays.equals(arr, ASC)) {
+            System.out.println("ascending");
+        } else if (Arrays.equals(arr, DESC)) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
         }
-        System.out.println(result);
     }
 }
