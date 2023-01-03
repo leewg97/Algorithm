adfca205c0be2eab8931f5ed8936468e434deb36
leewg97 <leewg97@naver.com>
2023-01-04 03:39:16 +0900

    Programmers 구명 보트

---
+++ src/Programmers/Lv2_1.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+import java.util.Arrays;
+
+// 구명 보트
+public class Lv2_1 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+
+        int index = 0;
+
+        for (int i = people.length - 1; i >= index; i--) {
+            if(people[i] + people[index] > limit) {
+                answer++;
+            } else {
+                answer++;
+                index++;
+            }
+        }
+        return answer;
+    }
+}
