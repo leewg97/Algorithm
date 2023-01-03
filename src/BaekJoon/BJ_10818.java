36715df165f17b619378dd435fc6c5aa49b99eb1
leewg97 <leewg97@naver.com>
2023-01-04 00:13:04 +0900

    BOJ 최소, 최대

---
+++ src/BaekJoon/BJ_10818.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 최소, 최대
+public class BJ_10818 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] + " " + arr[arr.length-1]);
+    }
+}
