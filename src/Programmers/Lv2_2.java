98731d2f68ecb316095fc8d926322ef6254eb3bc
leewg97 <leewg97@naver.com>
2023-01-04 03:39:21 +0900

    Programmers 큰 수 만들기

---
+++ src/Programmers/Lv2_2.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+// 큰 수 만들기
+public class Lv2_2 {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();
+
+        int index = 0;
+        int max = 0;
+
+        for ( int i = 0; i < number.length() - k; i++) {
+            max = 0;
+            for (int j = index; j <= i + k; j++) {
+                if (max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    index = j + 1;
+                }
+            }
+            sb.append(max);
+        }
+        return sb.toString();
+    }
+}
