2f54460a0684bb353407c569ec245c7218b93609
leewg97 <leewg97@naver.com>
2023-01-04 00:11:59 +0900

    BOJ 별 찍기 2

---
+++ src/BaekJoon/BJ_2439.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 별 찍기 2
+public class BJ_2439 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
