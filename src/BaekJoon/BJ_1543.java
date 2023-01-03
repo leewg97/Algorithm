e620048f9864116e136ebaae334666893279a4e3
leewg97 <leewg97@naver.com>
2023-01-04 04:49:19 +0900

    BOJ 문서 검색

--- src/BaekJoon/BJ_1543.java
+++ src/BaekJoon/BJ_1543.java
@@ -15,18 +15,32 @@ public class BJ_1543 {
         System.setIn(new ByteArrayInputStream(inputData.getBytes()));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder doc = new StringBuilder();
-        StringBuilder word = new StringBuilder();
-
-        doc.append(br.readLine());
-        word.append(br.readLine());
+        String docs = br.readLine();
+        String word = br.readLine();
 
         int count = 0;
-        int index = 0;
-        while ((index = doc.toString().indexOf(word.toString())) != -1) {
-            doc.delete(0, index + word.length());
-            count++;
+
+        for (int i = 0; i <= docs.length() - word.length(); i++) {
+            if (docs.substring(i, i + word.length()).equals(word)) { // subString(x, y) -> index x 위치에서 index y-1 위치 까지의 값을 리턴
+                count++;
+                i += word.length() - 1;
+            }
         }
         System.out.println(count);
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder doc = new StringBuilder();
+//        StringBuilder word = new StringBuilder();
+//
+//        doc.append(br.readLine());
+//        word.append(br.readLine());
+//
+//        int count = 0;
+//        int index = 0;
+//        while ((index = doc.toString().indexOf(word.toString())) != -1) {
+//            doc.delete(0, index + word.length());
+//            count++;
+//        }
+//        System.out.println(count);
     }
 }
