1d54b097ced45f81e60dec5d6e413ddac684a995
leewg97 <leewg97@naver.com>
2023-01-04 01:08:06 +0900

    BOJ 다이얼

--- src/BaekJoon/BJ_5622.java
+++ src/BaekJoon/BJ_5622.java
@@ -5,7 +5,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 상수
+// 다이얼
 public class BJ_5622 {
 
     public static void main(String[] args) throws IOException {
