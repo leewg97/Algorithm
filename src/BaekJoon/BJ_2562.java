87db7b3f75ca328c9307bb0e422567c0553b13df
leewg97 <leewg97@naver.com>
2023-01-04 00:12:04 +0900

    BOJ 최댓값

---
+++ src/BaekJoon/BJ_2562.java
@@ -0,0 +1,31 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 최댓값
+public class BJ_2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int index = 0;
+        int max = 0;
+        int count = 0;
+
+        for (int value : arr) {
+            count++;
+            if (value > max) {
+                max = value;
+                index = count;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
