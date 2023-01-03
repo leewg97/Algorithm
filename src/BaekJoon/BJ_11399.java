45a86653e2812d5d281d65866390c69dcf0c8a7c
leewg97 <leewg97@naver.com>
2023-01-04 01:08:18 +0900

    BOJ ATM

--- src/BaekJoon/BJ_11399.java
+++ src/BaekJoon/BJ_11399.java
@@ -10,36 +10,57 @@ import java.util.StringTokenizer;
 // ATM
 public class BJ_11399 {
 
-     public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
         String inputData = """
-        5
-        3 1 4 3 2
-        """;
-         System.setIn(new ByteArrayInputStream(inputData.getBytes()));
+                5
+                3 1 4 3 2
+                """;
+        System.setIn(new ByteArrayInputStream(inputData.getBytes()));
 
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-         int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-         int[] arr = new int[N];
+        int[] arr = new int[N];
 
-         for (int i = 0; i < N; i++) {
-             arr[i] = Integer.parseInt(st.nextToken());
-         }
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-         Arrays.sort(arr);
+        Arrays.sort(arr);
 
-         int prev = 0;
-         int sum = 0;
+        int prev = 0;
+        int sum = 0;
 
-         for (int i = 0; i < N; i++) {
-             sum += prev + arr[i];
+        for (int i = 0; i < N; i++) {
+            sum += prev + arr[i];
 
-             prev += arr[i];
-         }
-         System.out.println(sum);
-     }
+            prev += arr[i];
+        }
+        System.out.println(sum);
+
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         int N = Integer.parseInt(br.readLine());
+//         int[] arr = new int[N];
+//
+//         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//         for (int i = 0; i < N; i++) {
+//             arr[i] = Integer.parseInt(st.nextToken());
+//         }
+//
+//         Arrays.sort(arr);
+//
+//         int min = 0;
+//
+//         for (int i = 0; i < N; i++) {
+//             for (int j = 0; j < i + 1; j++) {
+//                 min += arr[j];
+//             }
+//         }
+//
+//         System.out.println(min);
+    }
 }
