f0e3a39a24baec5eed1bc9de863acc7ff01547b5
leewg97 <leewg97@naver.com>
2023-01-04 00:12:51 +0900

    BOJ OX 퀴즈

---
+++ src/BaekJoon/BJ_8958.java
@@ -0,0 +1,38 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// OX 퀴즈
+public class BJ_8958 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = br.readLine();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int count = 0;
+            int sum = 0;
+
+            for (int j = 0; j < arr[i].length(); j++) {
+                if (arr[i].charAt(j) == 'O') {
+                    count++;
+                } else {
+                    count = 0;
+                }
+                sum += count;
+            }
+            sb.append(sum).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
