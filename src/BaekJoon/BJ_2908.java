d8c38441f631f82d5e4d853138750acf58c4d803
leewg97 <leewg97@naver.com>
2023-01-04 00:12:23 +0900

    BOJ 상수

---
+++ src/BaekJoon/BJ_2908.java
@@ -0,0 +1,32 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 상수
+public class BJ_2908 {
+
+    public static void main(String[] args) throws IOException {
+        String inputData = """
+        839 237
+                """;
+        System.setIn(new ByteArrayInputStream(inputData.getBytes()));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        if (A > B) {
+            System.out.print(A);
+        } else {
+            System.out.print(B);
+        }
+
+    }
+
+}
