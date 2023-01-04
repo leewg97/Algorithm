18133b8c9ee6ebcf4a91ceb59bfeee37cadc28c4
leewg97 <leewg97@naver.com>
2023-01-05 04:10:54 +0900

    BOJ N과 M(1)

---
+++ src/BaekJoon/BJ_15649.java
@@ -0,0 +1,47 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// N과 M(1)
+public class BJ_15649 {
+
+    public static boolean[] visit;
+    public static int[] arr;
+    public static int n, m;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        visit = new boolean[n];
+
+        rec_func(0);
+        System.out.println(sb);
+    }
+
+    public static void rec_func(int k) {
+        if (k == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[k] = i + 1;
+                rec_func(k + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
