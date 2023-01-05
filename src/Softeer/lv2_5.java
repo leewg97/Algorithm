4c9928c4f565aa5054d287bfc0946c42892bfdbc
leewg97 <leewg97@naver.com>
2023-01-06 05:18:16 +0900

    Softeer 회의실 예약

---
+++ src/Softeer/lv2_5.java
@@ -0,0 +1,34 @@
+package Softeer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 회의실 예약
+public class lv2_5 {
+
+    public static int N, M, s, t;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        String[] room = new String[N];
+        String[] R = new String[M];
+        int[][] time = new int[M][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            room[i] = st.nextToken();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            R[i] = st.nextToken();
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+    }
+}
