e38ea6090df3f2eba66e381fc4f3491464fbc3bd
leewg97 <leewg97@naver.com>
2023-01-06 05:16:30 +0900

    BOJ 단어 정렬

---
+++ src/BaekJoon/BJ_1181.java
@@ -0,0 +1,38 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 단어 정렬
+public class BJ_1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[N];
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = st.nextToken();
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1.length() == o2.length()) return o1.compareTo(o2);
+            return o1.length() - o2.length();
+        });
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[0]).append('\n');
+        for (int i = 1; i < N; i++) {
+            if (!arr[i].equals(arr[i - 1])) {
+                sb.append(arr[i]).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
