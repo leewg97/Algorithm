1755e9dcbfab90289fec705602e5b197bff52c5d
leewg97 <leewg97@naver.com>
2023-01-07 02:03:32 +0900

    BOJ 명령 프롬프트

---
+++ src/BaekJoon/BJ_1032.java
@@ -0,0 +1,31 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 명령 프롬프트
+public class BJ_1032 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        char[] c = st.nextToken().toCharArray();
+
+        for (int i = 0; i < N - 1; i++) {   // 먼저 입력받은 c 제외
+            st = new StringTokenizer(br.readLine());
+            char[] file = st.nextToken().toCharArray();
+            for (int j = 0; j < c.length; j++) {
+                if (c[j] != file[j]) {
+                    c[j] = '?';
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
+
