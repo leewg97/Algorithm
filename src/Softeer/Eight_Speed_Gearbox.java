f23abc826b2115e043debaccc75b7d58e42f96b2
leewg97 <leewg97@naver.com>
2023-01-08 06:12:27 +0900

    [Softeer] 8단 변속기

--- src/Softeer/Eight_Speed_Gearbox.java
+++ src/Softeer/Eight_Speed_Gearbox.java
@@ -1,24 +1,17 @@
 package Softeer;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 // 8단 변속기
-public class lv2_1 {
+public class Eight_Speed_Gearbox {
 
     public static int[] asc = {1, 2, 3, 4, 5, 6, 7, 8};
     public static int[] desc = {8, 7, 6, 5, 4, 3, 2, 1};
 
     public static void main(String[] args) throws IOException {
-
-        String inputData = """
-        1 2 3 4 5 6 7 8
-        """;
-        System.setIn(new ByteArrayInputStream(inputData.getBytes()));
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
