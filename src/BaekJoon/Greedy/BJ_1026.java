19f3733e3814a19e59dd5cb36aaff4defd706d4c
leewg97 <leewg97@naver.com>
2023-01-08 06:14:39 +0900

    [BOJ] 보물

---
+++ src/BaekJoon/Greedy/BJ_1026.java
@@ -0,0 +1,41 @@
+package BaekJoon.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+// 보물
+public class BJ_1026 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+
+        Integer[] A = new Integer[N];
+        Integer[] B = new Integer[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);                             // 오름차순 정렬
+        Arrays.sort(B, Collections.reverseOrder()); // 내림차순 정렬
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += A[i] * B[i];
+        }
+
+        System.out.println(sum);
+    }
+}
