bc03693d2a727ff122c44e8d873641aebda4c82e
leewg97 <leewg97@naver.com>
2023-01-08 06:12:18 +0900

    [Programmers] 배열 자르기

--- src/Programmers/Cut_Array.java
+++ src/Programmers/Cut_Array.java
@@ -3,7 +3,7 @@ package Programmers;
 import java.util.Arrays;
 
 // 배열 자르기
-public class Lv0_1 {
+public class Cut_Array {
 
     public int[] solution(int[] numbers, int num1, int num2) {
         return Arrays.copyOfRange(numbers, num1, num2 + 1);
