5492d8f63997842dc69f5f788bcd3726f275c44a
leewg97 <leewg97@naver.com>
2023-01-08 06:11:59 +0900

    [Softeer] 지도 자동 구축

---
+++ src/Softeer/Automated_Map_Building.java
@@ -0,0 +1,26 @@
+package Softeer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 지도 자동 구축
+public class Automated_Map_Building {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int row = 2;
+        int dot = 4;
+
+        if (N == 0) {
+            System.out.println(4);
+        } else {
+            for (int i = 0; i < N; i++) {
+                row = row + (row - 1);
+                dot = row * row;
+            }
+            System.out.println(dot);
+        }
+    }
+}
