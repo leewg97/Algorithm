598b0c862797637d51f687204ab66beed9c87ecb
leewg97 <leewg97@naver.com>
2023-01-12 01:43:15 +0900

    [BOJ] 구구단

--- src/BaekJoon/Etc/BJ_2739.java
+++ src/BaekJoon/Etc/BJ_2739.java
@@ -1,21 +1,14 @@
-package BaekJoon;
+package BaekJoon.Etc;
 
 import java.util.Scanner;
 
 // 구구단
 public class BJ_2739 {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         int x = sc.nextInt();
-
         for(int i = 1; i < 10; i++){
             System.out.println(x + " * " + i + " = " + (x*i));
         }
-
-
     }
-
 }
