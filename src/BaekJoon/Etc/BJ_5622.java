45cbe3ce7938dd0cda81850e3e48b95ec59ef926
leewg97 <leewg97@naver.com>
2023-01-17 00:01:59 +0900

    [BOJ] 다이얼

--- src/BaekJoon/Etc/BJ_5622.java
+++ src/BaekJoon/Etc/BJ_5622.java
@@ -1,7 +1,6 @@
-package BaekJoon;
+package BaekJoon.Etc;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -9,11 +8,6 @@ import java.io.InputStreamReader;
 public class BJ_5622 {
 
     public static void main(String[] args) throws IOException {
-        String inputData = """
-        UNUCIC
-                """;
-        System.setIn(new ByteArrayInputStream(inputData.getBytes()));
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String str = br.readLine();
