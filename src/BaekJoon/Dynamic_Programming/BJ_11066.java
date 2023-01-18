2314980100561207cf9414ddbd0270f8c6d641a8
leewg97 <leewg97@naver.com>
2023-01-18 21:59:52 +0900

    [BOJ] 파일 합치기

--- src/BaekJoon/Dynamic_Programming/BJ_11066.java
+++ src/BaekJoon/Dynamic_Programming/BJ_11066.java
@@ -1,7 +1,6 @@
-package BaekJoon;
+package BaekJoon.Dynamic_Programming;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -9,16 +8,6 @@ import java.util.StringTokenizer;
 // 파일 합치기
 public class BJ_11066 {
     public static void main(String[] args) throws IOException {
-
-        String inputData = """
-        2
-        4
-        40 30 30 50
-        15
-        1 21 3 4 5 35 5 4 3 5 98 21 14 17 32
-        """;
-        System.setIn(new ByteArrayInputStream(inputData.getBytes()));
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringTokenizer st;
