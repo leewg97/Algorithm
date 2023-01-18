be63610d567e1c9adb41705773c2decfa0b93605
leewg97 <leewg97@naver.com>
2023-01-18 22:00:03 +0900

    [BOJ] 2*n 타일링

--- src/BaekJoon/Dynamic_Programming/BJ_11726.java
+++ src/BaekJoon/Dynamic_Programming/BJ_11726.java
@@ -1,7 +1,6 @@
-package BaekJoon;
+package BaekJoon.Dynamic_Programming;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -9,12 +8,6 @@ import java.io.InputStreamReader;
 public class BJ_11726 {
 
     public static void main(String[] args) throws IOException {
-
-        String inputData = """
-                9
-                """;
-        System.setIn(new ByteArrayInputStream(inputData.getBytes()));
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[1001];
