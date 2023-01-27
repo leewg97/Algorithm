5b661f7f887d7d990ee50076ef21a5ca9e6d4c81
leewg97 <leewg97@naver.com>
2023-01-28 00:08:40 +0900

    [BOJ] RGB 거리 * import 수정

--- src/BaekJoon/Dynamic_Programming/BJ_1149.java
+++ src/BaekJoon/Dynamic_Programming/BJ_1149.java
@@ -1,7 +1,6 @@
 package BaekJoon.Dynamic_Programming;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
