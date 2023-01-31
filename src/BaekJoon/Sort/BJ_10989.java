36396e3e2ea29930b102b4916da748538416a795
이원근 <leewg97@naver.com>
2023-01-31 11:23:36 +0900

    Update BJ_10989.java

--- src/BaekJoon/Sort/BJ_10989.java
+++ src/BaekJoon/Sort/BJ_10989.java
@@ -1,7 +1,6 @@
 package BaekJoon.Sort;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
