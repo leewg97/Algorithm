23c32066c69c6f551801dc9a3d0758ebe2a79652
leewg97 <leewg97@naver.com>
2023-05-16 21:21:28 +0900

    [BOJ] 그대로 출력하기 2

---
+++ src/BaekJoon/String/BJ_11719.java
@@ -0,0 +1,17 @@
+package BaekJoon.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 그대로 출력하기 2
+public class BJ_11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+
+    }
+}
