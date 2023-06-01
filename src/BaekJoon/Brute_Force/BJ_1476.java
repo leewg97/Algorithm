24d855cc279945ff4663ae24170c54e554479add
leewg97 <leewg97@naver.com>
2023-06-02 00:01:13 +0900

    [BOJ] 날짜계산

--- src/BaekJoon/Brute_Force/BJ_1476.java
+++ src/BaekJoon/Brute_Force/BJ_1476.java
@@ -3,28 +3,31 @@ package BaekJoon.Brute_Force;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 날짜계산
 public class BJ_1476 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int e = 1, s = 1, m = 1;
-        int E = Integer.parseInt(br.readLine());
-        int S = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int E = 15;
+        int S = 28;
+        int M = 19;
 
-        for (int i = 1; ; i++) {
-            if (e == E && s == S && m == M) {
-                System.out.println(i);
-                break;
-            }
-            e += 1;
-            s += 1;
-            m += 1;
-            if (e == 16) e = 1;
-            if (s == 29) s = 1;
-            if (m == 20) m = 1;
-        }
+        int year = 0;
 
+        int e = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if (e == E) e = 0;
+        if (s == S) s = 0;
+        if (m == M) m = 0;
+
+        do {
+            year += 1;
+        } while (year % E != e || year % S != s || year % M != m);
+
+        System.out.println(year);
     }
 }
