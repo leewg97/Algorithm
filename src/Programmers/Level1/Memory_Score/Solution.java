53aca54aa179c148cba630c19dfdbf036573a5d0
leewg97 <leewg97@naver.com>
2024-07-19 12:38:51 +0900

    [프로그래머스] 추억 점수

---
+++ src/Programmers/Level1/Memory_Score/Solution.java
@@ -0,0 +1,28 @@
+package Programmers.Level1.Memory_Score;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String, Integer> memoryMap = new HashMap<>();
+
+        for (int i = 0; i < name.length; i++) {
+            memoryMap.put(name[i], yearning[i]);
+        }
+
+        int[] answer = new int[photo.length];
+
+        for (int i = 0; i < photo.length; i++) {
+            int score = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                if (memoryMap.containsKey(photo[i][j])) {
+                     score += memoryMap.get(photo[i][j]);
+                }
+            }
+            answer[i] = score;
+        }
+
+        return answer;
+    }
+}
