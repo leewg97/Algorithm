3dda16e6c30576f2ea5a1e7ff2da1baa46e178a5
leewg97 <leewg97@naver.com>
2024-07-22 11:02:17 +0900

    [프로그래머스] 약수의 합 * https://school.programmers.co.kr/learn/courses/30/lessons/12928

---
+++ src/Programmers/Level1/약수의_합/Solution.java
@@ -0,0 +1,15 @@
+package Programmers.Level1.약수의_합;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) if (n % i == 0) answer += i;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution(12);
+        System.out.println(result);
+    }
+}
